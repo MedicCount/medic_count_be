2e15fa19cab5a4ee212aa21d9cee735fe32b3f38
xeff09 <wongchaita03@gmail.com>
2024-03-30 16:15:19 +0700

    add: yolov5, pretrained model

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,9 @@
-FROM python:3.10.13
+FROM python:3.10.7
 WORKDIR /app
 COPY . /app
 RUN pip install -r ./app/requirements.txt
+RUN apt-get update && apt-get install -y libgl1-mesa-glx
+RUN apt-get update
+RUN apt-get install 'ffmpeg'\
+    'libsm6'\ 
+    'libxext6'  -y
--- app/main.py
+++ app/main.py
@@ -2,25 +2,23 @@ from fastapi import FastAPI, File, UploadFile
 from PIL import Image
 import numpy as np
 import io
+import os
+import subprocess
 
 import tensorflow as tf
-import joblib
 
 app = FastAPI()
 
-# load detection model
-# loaded_model = joblib.load("model.joblib")
+model_path = 'app/model/best.pt'
 
 def detect_count_image(image):
-    return 112
-
     pil_image = Image.open(io.BytesIO(image))
-    pil_image = pil_image.resize((224, 224))
-    resized_image = np.array(pil_image)
-    resized_image = resized_image.reshape(-1, 224, 224, 3)
-
-    # detection = loaded_model.predict(resized_image).tolist()[0]
-    # return detection
+    pil_image.save('app/model/image/tmp.jpg')
+    image_path = 'app/model/image/tmp.jpg'
+    
+    command = f"python app/model/yolov5/detect.py --source \"{image_path}\" --weights {model_path} --save-txt"
+    subprocess.run(command, shell=True)
+    return 1123
 
 
 @app.get("/")
